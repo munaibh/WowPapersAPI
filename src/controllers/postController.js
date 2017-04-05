7c5715240e79b435906511f2ba300fc11102f0ff
Munaib Hussain <munaibh@gmail.com>
2017-04-05 23:11:47 +0100

    Refactored Post Retrieval Functions

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -24,122 +24,108 @@ controller.create = (req,res,next) => {
 
 }
 
-// Get Post
-controller.get = (req,res,next) => {
-
-  const page  = req.query.page || 1
-  const limit = Number(req.query.limit) || 5
-
-  db.Post.find({}).limit(limit).skip(limit*(page-1))
-    .then(posts => {
+// Get Paginated Content Function (HELPER)
+const getPaginated = (query, id, page, limit) => {
+  return new Promise((resolve, reject) => {
+    query.limit(limit).skip(limit*(page-1))
+      .then(posts => {
         if(posts.length === 0) throw new Error("No Results")
         return posts
       })
-    .then(posts => {
-      async.map(posts, (item, cb) => {
-        db.Like.find({_post: item._id}, (err, likes) => {
-          let exists = []
-          item = item.toObject()
-          item.like_count = likes.length || 0
-          if(req.id) exists = likes.filter((liked) => liked._creator == req.id)
-          item.liked = (exists.length > 0)
-          cb(null, item)
+      .then(posts => {
+        async.map(posts, (item, cb) => {
+          db.Like.find({_post: item._id}, (err, likes) => {
+            let exists = []
+            item = item.toObject()
+            item.like_count = likes.length || 0
+            if(id) exists = likes.filter((liked) => liked._creator == id)
+            item.liked = (exists.length > 0)
+            cb(null, item)
+          })
+        }, (err, results) => {
+        query.skip(limit*page).count()
+            .then(count => {
+              resolve({
+                success: true,
+                data: results,
+                next_count: count
+              })
+            })
         })
-      }, (err, results) => {
-        db.Post.find({}).skip(limit*page).count()
-          .then(count => {
-            res.status(200).json({
+      })
+      .catch(err => reject(err))
+    })
+}
+
+// Get Single Result (Helper)
+var getOneResult = function(query, id) {
+  return new Promise((resolve, reject) => {
+    query
+      .then(post => {
+          if(!post) throw new Error("No Results")
+          return post
+        })
+      .then(post => {
+          db.Like.find({_post: post._id}, (err, likes) => {
+            let exists = []
+            post = post.toObject()
+            post.like_count = likes.length || 0
+            if(id) exists = likes.filter((liked) => liked._creator == id)
+            post.liked = (exists.length > 0)
+            resolve({
               success: true,
-              data: results,
-              next_count: count
+              data: post,
             })
           })
       })
+      .catch(err => reject(err))
     })
+}
+
+// Get All Posts
+controller.get = (req,res,next) => {
+  // Get Variables
+  const id    = req.id
+  const page  = req.query.page || 1
+  const limit = Number(req.query.limit) || 5
+  // Pass to Function
+  getPaginated(db.Post.find({}), id, page, limit)
+    .then(result => res.status(200).json(result))
     .catch(err => next(err))
 }
 
 // Search Posts
 controller.searchByName = (req,res,next) => {
-
+  // Get Variables
+  const id    = req.id
+  const name  = req.params.name
   const page  = req.query.page || 1
   const limit = Number(req.query.limit) || 5
-  const name  = req.params.name
-
-  db.Post.find({title: { "$regex": req.params.name, "$options": '?i' }}).limit(limit).skip(limit*(page-1))
-    .then(posts => {
-        if(posts.length === 0) throw new Error("No Results")
-        return posts
-      })
-    .then(posts => {
-      async.map(posts, (item, cb) => {
-        db.Like.find({_post: item._id}, (err, likes) => {
-          let exists = []
-          item = item.toObject()
-          item.like_count = likes.length || 0
-          if(req.id) exists = likes.filter((liked) => liked._creator == req.id)
-          item.liked = (exists.length > 0)
-          cb(null, item)
-        })
-      }, (err, results) => {
-        db.Post.find({title: { "$regex": req.params.name, "$options": '?i' }}).skip(limit*page).count()
-          .then(count => {
-            res.status(200).json({
-              success: true,
-              data: results,
-              next_count: count
-            })
-          })
-      })
-    })
+  // Pass to Function
+  getPaginated(db.Post.find({title: { "$regex": name, "$options": '?i' }}), id, page, limit)
+    .then(result => res.status(200).json(result))
     .catch(err => next(err))
 }
 
 // Get Single Post By ID
 controller.getSingleById = (req,res,next) => {
-
-  db.Post.findById(req.params.id)
-    .then(post => {
-        if(!post) throw new Error("No Results")
-        return post
-      })
-    .then(post => {
-        db.Like.find({_post: post._id}, (err, likes) => {
-          let exists = []
-          post = post.toObject()
-          post.like_count = likes.length || 0
-          if(req.id) exists = likes.filter((liked) => liked._creator == req.id)
-          post.liked = (exists.length > 0)
-          res.status(200).json({
-            success: true,
-            data: post,
-          })
-        })
-    })
+  // Get Variables
+  var id     = req.id
+  var postId = req.params.id
+  // Pass to Function
+  getOneResult(db.Post.findById(req.params.id), id)
+    .then(result => res.status(200).json(result))
     .catch(err => next(err))
 }
 
 // Get Single Post By Slug
 controller.getSingleBySlug = (req,res,next) => {
-
-  db.Post.findOne({concat_title: req.params.slug})
-    .then(post => {
-        if(!post) throw new Error("No Results")
-        return post
-      })
-    .then(post => {
-        db.Like.find({_post: post._id}, (err, likes) => {
-          let exists = []
-          post = post.toObject()
-          post.like_count = likes.length || 0
-          if(req.id) exists = likes.filter((liked) => liked._creator == req.id)
-          post.liked = (exists.length > 0)
-          res.status(200).json({
-            success: true,
-            data: post,
-          })
-        })
-    })
+  // Get Variables
+  var id   = req.id
+  var slug = req.params.slug
+  // Pass to Function
+  getOneResult(db.Post.findOne({concat_title: req.params.slug}), id)
+    .then(result => res.status(200).json(result))
     .catch(err => next(err))
 }
 
